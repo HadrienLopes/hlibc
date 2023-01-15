1b2dc3522979ee135de8ebe72f62e8ef239837cf
HadrienLopes <hadriendev_@hotmail.com>
2023-01-15 23:46:32 +0100

    ft_strlen.c optimized

--- ft_strlen.c
+++ ft_strlen.c
@@ -12,12 +12,26 @@
 
 #include "libft.h"
 
+#define QUADMASK80 0x8080808080808080
+#define QUADMASK01 0x0101010101010101
+
 size_t	ft_strlen(const char *s)
 {
 	size_t i;
+	char *str;
+	long long int quadword;	
+	long long int *quadword_p;
 
 	i = 0;
-	while (s && s[i])
+	quadword_p = (long long int*)s;
+	quadword = *quadword_p;
+	while (!((quadword - QUADMASK01) & ~quadword & QUADMASK80))
+	{
+		quadword_p++;
+		quadword = *quadword_p;
+	}
+	str = (char*)quadword_p;
+	while (str[i] != 0)
 		i++;
-	return (i);
+	return (str - s + i);
 }
