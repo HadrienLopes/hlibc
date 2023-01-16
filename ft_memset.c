1e7d381979f171abe5de7b642fa7b5ecce3e26b4
HadrienLopes <hadriendev_@hotmail.com>
2023-01-16 23:40:18 +0100

    ft_memset optimized

--- ft_memset.c
+++ ft_memset.c
@@ -14,15 +14,44 @@
 
 void	*ft_memset(void *str, int c, size_t n)
 {
-	char	*str_2;
-	size_t	i;
+	size_t i;
+	unsigned long long int quadword;
+	unsigned long long int *quadword_p;
 
 	i = 0;
-	str_2 = str;
-	while (i < n)
+	quadword = 0;
+	quadword = quadword | c;
+	quadword = (quadword << 8) | quadword;
+	quadword = (quadword << 16) | quadword;
+	quadword = (quadword << 32) | quadword;
+	quadword_p = (unsigned long long int*)str;
+	while (i < (n / 8))
 	{
-		str_2[i] = (char)c;
+		*quadword_p = quadword;
+		quadword_p++;
 		i++;
 	}
-	return (str);
+	if (n & 0x4)
+	{
+		int *i_p = (int*)quadword_p;
+		*i_p = (int)quadword;
+		i_p++;
+		quadword_p = (unsigned long long int*)i_p;
+	}
+	if (n & 0x2)
+	{
+		short *s_p = (short*)quadword_p;	
+		*s_p = (short)quadword;
+		s_p++;
+		quadword_p = (unsigned long long int*)s_p;
+	}
+	if (n & 0x1)
+	{
+		char *c_p = (char*)quadword_p;
+		*c_p = (char)quadword;
+		c_p++;
+		quadword_p = (unsigned long long int*)c_p;
+
+	}
+	return (str);	
 }
