63942a18030cb6869d8b1e247abebee9a2bd063f
HadrienLopes <hadriendev_@hotmail.com>
2023-01-25 12:53:27 +0100

    updated ft_split and ft_strplit.c

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -1,83 +1,60 @@
 #include "libft.h"
 
-static void	free_str_mat(char **str_mat, int max)
+char	**ft_strsplit(char const *s, char c)
 {
-	int i;
+	char		*ptr;
+	char		*ptr2;
+	char		**table;
+	unsigned int	count;
+	unsigned int i;
 
-	i = 0;
-	while (i < max)
-	{
-		free(str_mat[i]);
-		i++;
-	}
-	free(str_mat);
-}
-
-static char	**fill_it_well(char **str_mat, char const *s, char c)
-{
-	int i;
-	int j;
-	int k;
-
-	i = 0;
-	j = 0;
-	k = 0;
-	while (s[i])
-	{
-		if (s[i] == c)
-			j = 0;
-		else
-		{
-			j++;
-			if (s[i + 1] == c || s[i + 1] == '\0')
-			{
-				if (!(str_mat[k] = (char*)malloc(sizeof(char) * (j + 1))))
-				{
-					free_str_mat(str_mat, k);
-					return (NULL);
-				}
-				str_mat[k] = ft_strncpy(str_mat[k], (s + i - j + 1), j);
-				k++;
-			}
-		}
-		i++;
-	}
-	return (str_mat);
-}
-
-static int	count_str(char const *s, char c)
-{
-	int i;
-	int count;
-	int counted;
-
-	i = 0;
 	count = 0;
-	counted = 0;
-	while (s[i])
+	ptr = (char*)s;
+	if (s == NULL)
+		return (NULL);
+	while (*ptr == c)
+		ptr++;
+	if (*ptr == '\0')
+		return (NULL);
+	while (*ptr != '\0')
 	{
-		if (s[i] == c)
-			counted = 0;
-		else if (counted == 0)
+		if (!(ptr2 = ft_strchr(ptr, c)))
 		{
 			count++;
-			counted = 1;
+			break ;
 		}
-		i++;
+		if (ptr2 - ptr)	
+			count++;
+		ptr = ++ptr2;
 	}
-	return (count);
+	if (!(table = (char**)malloc(sizeof(char*) * (count + 1))))
+		return (NULL);
+	i = 0;
+	ptr = (char*)s;
+	while (*ptr != '\0')
+	{
+		if (!(ptr2 = ft_strchr(ptr, c)))
+		{
+			if (!(table[i] = ft_strdup(ptr)))
+			{
+				//ft_tabdeli(*table, i - 1);
+				return (NULL);
+			}
+			i++;
+			break ;
+		}
+		if (ptr2 - ptr)
+		{
+			if (!(table[i] = ft_strndup(ptr, ptr2 - ptr)))
+			{
+				//ft_tabdeli(*table, i );
+				return (NULL);
+			}
+			i++;
+		}
+		ptr = ++ptr2;
+	}
+	table[i] = NULL;
+	return (table);
 }
 
-char			**ft_strsplit(char const *s, char c)
-{
-	int		count;
-	char	**str_mat;
-
-	if (!(count = count_str(s, c)))
-		return (NULL);
-	if (!(str_mat = (char**)malloc(sizeof(char*) * (count + 1))))
-		return (NULL);
-	str_mat = fill_it_well(str_mat, s, c);
-	str_mat[count] = NULL;
-	return (str_mat);
-}
