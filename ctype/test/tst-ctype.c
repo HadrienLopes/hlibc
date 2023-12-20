f288471811a67bcecef7f12a67b12d9d4d676ef6
Hadrien Moulherat <hadrien.moulherat@etudiant.univ-rennes1.fr>
2023-12-20 18:48:11 +0100

    Added check rule on makefile

--- ctype/test/tst-ctype.c
+++ ctype/test/tst-ctype.c
@@ -6,18 +6,22 @@
 /*   hadrien.moulherat@etudiant.univ-rennes1.fr     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 12:33:01 by hadrien           #+#    #+#             */
-/*   Updated: 2023/12/17 21:11:22 by hadrien          ###   ########.fr       */
+/*   Updated: 2023/12/20 18:46:05 by hadrien          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <h_ctype.h>
-#include <h_limits.h>
-#include <h_stdlib.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /* Defined ctype axioms. */
+#define ISALNUM(c) (ISALPHA(c) || ISDIGIT(c))
 #define ISUPPER(c) (c >= 'A' && c <= 'Z')
 #define ISLOWER(c) (c >= 'a' && c <= 'z')
 #define ISALPHA(c) (ISUPPER(c) || ISLOWER(c))
+#define ISASCII(c) (c >= 0 && c <= 127)
+#define ISDIGIT(c) (c >= '0' && c <= '9')
 
 int main(int argc, char *argv[])
 {
@@ -27,13 +31,31 @@ int main(int argc, char *argv[])
 	err = 0;
 	/* We only test for the interval [0; UCHAR_MAX]
 	   because the parameter c is everytime casted into unsigned char. */
-	for (c = 0; c < UCHAR_MAX; i++)
+	for (c = 0; c < UCHAR_MAX; c++)
 	{
-		if (h_isalpha(c) != ISALPHA(c))
+		if (h_isalnum(c) != ISALNUM(c))
 		{
-			print_error("h_isalpha", c);
+			printf("h_isalpha: %d\n", (int)c);
 			err++;
 		}
+		if (h_isalpha(c) != ISALPHA(c))
+		{
+			printf("h_isalpha: %d\n", (int)c);
+			err++;
+		}	
+		if (h_isascii(c) != ISASCII(c))
+		{
+			printf("h_isascii: %d\n", (int)c);
+			err++;
+		}	
+		
+		if (h_isdigit(c) != ISDIGIT(c))
+		{
+			printf("h_isdigit: %d\n", (int)c);
+			err++;
+		}
+		
 	}
+	printf("final errors: %d\n", err);
 	return (err ? EXIT_FAILURE : EXIT_SUCCESS);
 }
